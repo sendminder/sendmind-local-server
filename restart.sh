8dc6fee8502ad7280e56dd5349494612d644aab5
sendminder <sendminder@gmail.com>
2025-03-22 20:56:21 +0900

    update

--- restart.sh
+++ restart.sh
@@ -1,2 +1,9 @@
 sudo docker compose -f ./docker-compose.yaml down
 sudo docker compose -f ./docker-compose.yaml up -d
+
+docker exec -it netdata sh -c 'cat > /etc/netdata/go.d/prometheus.conf << EOF
+jobs:
+  - name: sendmind-hub
+    url: http://sendmind-hub:8080/metrics
+    collect_response_time: yes
+EOF'
